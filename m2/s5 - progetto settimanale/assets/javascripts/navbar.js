db67ef7d87e66d894045ffcd04a8e0d73b33870c
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-03-13 15:14:40 +0100

    Es.1

--- m2/s5 - progetto settimanale/assets/javascripts/navbar.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.querySelector('.barranav');
-window.onscroll = () => {
-    if (window.scrollY > 300) {
-        navbar.classList.add('nav-active');
-    } else {
-        navbar.classList.remove('nav-active');
-    }
-};
